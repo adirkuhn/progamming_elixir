84f8762689a6d659f23d1a161cb20ce710ebd303
Adir Kuhn <adir@sam-media.com>
2016-10-03 23:16:52 -0300

    project

---
+++ lib/programming_elixir.ex
@@ -0,0 +1,2 @@
+defmodule ProgrammingElixir do
+end
---
+++ test/programming_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule ProgrammingElixirTest do
+  use ExUnit.Case
+  doctest ProgrammingElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
